e1ca20664274cb9164840df17a67750fa1f5f8a5
Teunemans <tevanbostelen@gmail.com>
2023-09-16 22:47:53 +0100

    Epic website night

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const h1 = document.getElementById('title');
 while (true) {
-    h1.innerHTML = "De grote Informatica  Website"
-    setTimeout(donothing, 1000);
-    h1.innerHTML = "De grote Informatica  Website_"
-}
+    h1.innerHTML = "De grote Informatica  Website";
+    setTimeout(donothing, 1);
+    h1.innerHTML = "De grote Informatica  Website_";
+};
