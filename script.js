3906d5b26febfaafa47af95467fa9ea6871e4818
Teunemans <tevanbostelen@gmail.com>
2023-09-18 21:45:20 +0100

    Lijpe animaties

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const h1 = document.getElementById('title');
-while (true) {
-    h1.innerHTML = "De grote Informatica  Website";
-    setTimeout(donothing, 1);
-    h1.innerHTML = "De grote Informatica  Website_";
-};
+var i = 0;
+
+/* De tekst die getypt moet worden */
+var txt = 'Hello World!';
+
+/* Hoe snel de letters worden getypt */
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+const test = document.getElementById("test");
+test = document.innerHTML = "hallo meneer!";
