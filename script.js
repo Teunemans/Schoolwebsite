d2a12588b5e4ca139e793983843c7bedbdf9207c
Teunemans <tevanbostelen@gmail.com>
2023-10-27 09:40:05 +0200

    Login JS

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function typeWriter() {
 
 const test = document.getElementById("test");
 test = document.innerHTML = "hallo meneer!";
+
+// Script voor Login
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
