5e85afb6ecb5f179824cbf29c297c4c28c052e70
Teunemans <tevanbostelen@gmail.com>
2023-11-02 14:58:27 +0100

    lseos

--- script.js
+++ script.js
@@ -25,12 +25,14 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
-    const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
+    if (username === "user" && password === "password") {
+      window.location.href = "geheim.html";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+while (true) {
+
+}
