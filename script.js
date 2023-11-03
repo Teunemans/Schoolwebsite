49d2ae0d0dd20695a74383d0fbde6c9a3a0e41ce
Teunemans <117749708+Teunemans@users.noreply.github.com>
2023-11-03 12:33:26 +0100

    Update script.js
    code is now password

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
-var i = 0;
-
-/* De tekst die getypt moet worden */
-var txt = 'Hello World!';
-
-/* Hoe snel de letters worden getypt */
-var speed = 100;
-
+// Script voor het type-effect
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("typewriter").innerHTML += txt.charAt(i);
@@ -21,7 +13,7 @@ test = document.innerHTML = "hallo meneer!";
 // Script voor Login pagina
 function checkvalue(){
   var input = document.getElementById("username").value;
-  if (input == "deGeheimeCode") {
+  if (input == "password") {
     window.location.href = "geheim.html";
   }
   else {
